2ef9f8777074556947a7123df6f428f66ff940b2
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-04-06 15:34:35 +0900

    Add files via upload

---
+++ Practice02.java
@@ -0,0 +1,19 @@
+//백준 문제 단계별로 풀어보기2-1001번 문제
+import java.util.Scanner;
+public class Practice02 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int a,b;
+
+        System.out.printf("정수를 입력하세요: ");
+        a = s.nextInt();
+
+        System.out.printf("정수를 입력하세요: ");
+        b = s.nextInt();
+
+        System.out.printf("%d",a-b);
+
+
+    }
+}
