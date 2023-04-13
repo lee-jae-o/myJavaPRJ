da160279e761b21566f10cc9e88b0987ef355183
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-04-13 14:35:19 +0900

    Add files via upload

---
+++ src/main/java/Practice05.java
@@ -0,0 +1,12 @@
+//백준 10926번
+import java.util.Scanner;
+
+public class Practice05 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print(" ");
+        String a = s.nextLine();
+        String str = a + "??!";
+        System.out.println(str);
+    }
+}
