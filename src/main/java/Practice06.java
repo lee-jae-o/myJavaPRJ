888af209bdf86a8178e65f94d4239dc1eee97d64
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-04-14 19:27:35 +0900

    Add files via upload

---
+++ src/main/java/Practice06.java
@@ -0,0 +1,11 @@
+//백준 2743번 문제
+import java.util.Scanner;
+public class Practice06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        int length = a.length();
+        System.out.print(length);
+    }
+}
+
