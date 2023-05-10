0b6357b96e7c222650cb44f89509a23f77bbbc36
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-05-10 12:00:03 +0900

    Add files via upload

---
+++ src/main/java/Test001.java
@@ -0,0 +1,15 @@
+public class Test001 {
+    public static void main(String[] args) {
+        String log = "123.123.123.123 - - [12/Apr/2018:17:03:50 +0900] \"GET /api/aaaa HTTP/1.1\" 200 34 1468 \"https://m.naver.com\" \"Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E216 NAVER(inapp; search; 580; 8.6.3; 7)\"";
+
+        String ip[] = log.split(" ");
+        System.out.println(" ip 출력:" + ip[0]);
+
+        ip = log.split(" ");
+        System.out.println("시간:" + ip[3]);
+        String time;
+       time = ip[3].replace("[","");
+        System.out.println("시간:"+ time);
+
+    }
+}
