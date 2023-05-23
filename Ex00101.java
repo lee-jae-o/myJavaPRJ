e98553000b33dcc1987ccafa89fe82a36ae43550
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-05-23 14:51:37 +0900

    Add files via upload

---
+++ Ex00101.java
@@ -0,0 +1,9 @@
+public class Ex00101 {
+    public class child extends Car{
+        @Override
+        void upSpeed(int value) {
+            super.upSpeed(value);
+            return ;
+        }
+    }
+}
