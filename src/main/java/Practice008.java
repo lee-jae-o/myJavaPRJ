7df2ee00baca2572fed8de469bb47ba8476c1438
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-08-02 21:48:13 +0900

    Add files via upload

---
+++ src/main/java/Practice008.java
@@ -0,0 +1,17 @@
+public class Practice008 {
+    public static void main(String[] args) {
+        String ssn = "9609081234567";
+        char sex = ssn.charAt(6);
+        switch (sex){
+            case '1':
+            case '3':
+                System.out.println("남자입니다.");
+                break;
+
+            case '2':
+            case '4':
+                System.out.println("여자입니다.");
+                break;
+        }
+    }
+}
