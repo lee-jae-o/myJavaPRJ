54d382350b9fae49143c94f321f2b904ee156ffb
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-08-09 21:54:01 +0900

    Add files via upload

---
+++ src/main/java/Practice016.java
@@ -0,0 +1,12 @@
+public class Practice016 {
+    public static void main(String[] args) {
+
+        int a,b;
+        a=9; b=4;
+
+        System.out.println(a%b);
+
+        double g = (double)a / b;
+        System.out.println(g);
+    }
+}
