f2e6dcdea897a0b1a045676879f6200749c13829
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-08-17 22:32:43 +0900

    Add files via upload

---
+++ src/main/java/Practice017.java
@@ -0,0 +1,13 @@
+public class Practice017 {
+    public static void main(String[] args) {
+
+        boolean a;
+        int c = 1;
+        int d = 2;
+
+        int hap = c + d;
+        a=hap==4;
+
+        System.out.println("hap is 4:"+a);
+    }
+}
