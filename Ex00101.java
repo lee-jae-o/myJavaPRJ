18c270827c9942d8db831ae2a81582c42b0b3974
lee-jae-o <127208195+lee-jae-o@users.noreply.github.com>
2023-08-22 14:56:03 +0900

    Delete Ex00101.java

--- Ex00101.java
+++
@@ -1,9 +0,0 @@
-public class Ex00101 {
-    public class child extends Car{
-        @Override
-        void upSpeed(int value) {
-            super.upSpeed(value);
-            return ;
-        }
-    }
-}
